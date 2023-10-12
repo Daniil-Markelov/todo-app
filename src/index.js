aad2f52fe18224a544f33d6c751039d5330eaed1
Daniil Markelov <N00202705@student.iadt.ie>
2023-10-12 13:02:08 +0100

    fianl

--- src/index.js
+++ src/index.js
@@ -15,3 +15,14 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//const deleteTodo = (id) => {
+ // const newList =list.filter((item) => {
+
+ // return item.id !== id;
+
+ // });
+ 
+ // setList(newList);
+//}
+  
+  
